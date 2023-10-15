df9c986005e4bdb86007864b2cbf25d70fc9b07d
richmr <richmr@users.noreply.github.com>
2023-10-14 19:08:17 -0700

    fumblings in the dark

---
+++ src/pyencountersim/Creatures/__init__.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+
+class CreatureState(BaseModel):
+    id:int
+    name:str
+    AC:int
+    CurrentHP:int
+    MaxHP:int
+
+
+
+class Creature:
+
+    _currentID = 1
+
+    def __init__(self) -> None:
+        self.state = CreatureState(id=self._currentID)
+        self._currentID += 1
+        self._startTurnStack = []
+        self._endTurnStack = []
+        self._target = None
+        pass
+
+    def takeTurn(self):
+        pass
+
+    def startTurn(self):
+        pass
+
+    def takeDamage(self, dmg_object):
+        pass
+
+    def attack(self):
+        pass
+
+    def action(self):
+        pass
+
+    def move(self):
+        pass
+
+    def bonusAction(self):
+        pass
+    
+
+
