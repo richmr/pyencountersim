09585bb45c6ed074aac9369991b5ebc42e2acf31
Michael Rich <richmr2174@gmail.com>
2023-10-21 14:19:22 -0700

    Stats update

--- src/pyencountersim/Creatures/__init__.py
+++ src/pyencountersim/Creatures/__init__.py
@@ -1,20 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-class CreatureState(BaseModel):
+class CreatureStats(BaseModel):
     id:int
-    name:str
-    AC:int
-    CurrentHP:int
-    MaxHP:int
-
-
+    name:str = Field(default="Creature")
+    level:int = Field(default=1)
+    AC:int = Field(default=10)
+    HP:int = Field(default=0)
+    Str:int = Field(default=10)
+    Str_mod:int = Field(default=0)
+    Str_savings_mod:int = Field(default=0)
+    Dex:int = Field(default=10)
+    Dex_mod:int = Field(default=0)
+    Dex_savings_mod:int = Field(default=0)
+    Con:int = Field(default=10)
+    Con_mod:int = Field(default=0)
+    Con_savings_mod:int = Field(default=0)
+    Int:int = Field(default=10)
+    Int_mod:int = Field(default=0)
+    Int_savings_mod:int = Field(default=0)
+    Wis:int = Field(default=10)
+    Wis_mod:int = Field(default=0)
+    Wis_savings_mod:int = Field(default=0)
+    Cha:int = Field(default=10)
+    Cha_mod:int = Field(default=0)
+    Cha_savings_mod:int = Field(default=0)
+    
 
 class Creature:
 
     _currentID = 1
 
     def __init__(self) -> None:
-        self.state = CreatureState(id=self._currentID)
+        self.state = CreatureStats(id=self._currentID)
         self._currentID += 1
         self._startTurnStack = []
         self._endTurnStack = []
