b89bed8ecc38cabfca3b5a6e57c5446e433ca988
Michael Rich <richmr2174@gmail.com>
2023-10-25 19:48:53 -0700

    Conitnue refactor

--- src/pyencountersim/Weapons/__init__.py
+++ src/pyencountersim/Weapons/__init__.py
@@ -2,7 +2,7 @@
 from enum import Enum, auto
 import warnings
 
-# from pyencountersim.Creatures import Creature # Uh oh, circular imports here.  May need to separate basics out
+from pyencountersim.Creatures import Creature 
 
 
 
@@ -23,5 +23,31 @@ Otherwise that attack object can get hopelessly confusing
 """
 
 
+class TargetAffector:
+    """
+    All things that affect Creatures should use this as base class
+    These can be attacks, heals, banes, boons
+    """
+
+    def __init__(self) -> None:
+        # placeholder
+        pass
+
+    def affect(self, targetObj:Creature) -> None:
+        """
+        This method is intended to be overridden by spells or other things that affect Creatures
+        The various methods of the Creature are called to apply effects
+        """
+        warnings.warn("Base 'TargetAffector.affect()' called.  This does nothing.", RuntimeWarning)
+        return
     
+    def attack(self, targetObj:Creature) -> None:
+        """
+        This method is intended to be overridden by weapons, spells, or other things that damage Creatures
+        The various methods of the Creature are called to apply damage and effects
+
+        Really no difference between this and 'affect' but called 'attack' for clarity of intent
+        """
+        warnings.warn("Base 'TargetAffector.attack()' called.  This does nothing.", RuntimeWarning)
+        return    
     
